49ab5dc704e639fae36ba9da85a887de86759c76
achraffawzi <achraffawzi224a@gmail.com>
2021-08-01 17:59:28 +0200

    commiting

--- FRONT_END/src/views/UserDashboard.vue
+++ FRONT_END/src/views/UserDashboard.vue
@@ -109,13 +109,11 @@
             <v-text-field
             label="number of calories"
               v-model="numCals"
-              prepend-icon="number"
               :rules="numCalsRules"
             ></v-text-field>
             <v-textarea
               label="description"
               v-model="description"
-              prepend-icon="folder"
               :rules="descriptionRules"
             ></v-textarea>
 
@@ -128,6 +126,7 @@
               offset-y
               max-width="290px"
               min-width="auto"
+              
             >
               <template v-slot:activator="{ on, attrs }">
                 <v-text-field
@@ -135,14 +134,14 @@
                   label="Date"
                   hint="MM/DD/YYYY format"
                   persistent-hint
-                  prepend-icon="mdi-calendar"
                   v-bind="attrs"
                   @blur="date = parseDate(dateFormatted)"
                   v-on="on"
+                  class="mb-6"
                 ></v-text-field>
               </template>
               <v-date-picker
-                v-model="date"
+                v-model="mealDate"
                 no-title
                 @input="menu1 = false"
               ></v-date-picker>
@@ -151,6 +150,7 @@
               ampm-in-title
               format="24hr"
               scrollable
+              v-model="time"
             ></v-time-picker>
           </v-form>
         </v-card-text>
@@ -162,8 +162,6 @@
           <v-btn
             color="primary"
             text
-            :loading="loading"
-            @click="submit"
             class="text-lowercase"
           >
             Add
@@ -178,9 +176,13 @@
 <script>
 export default {
   name: "UserDashboard",
-  data() {
+  data(vm) {
     return {
       searchingMeal: "",
+      numCals: "",
+      description: "",
+      mealDate: "",
+      time: "",
       drawer: null,
       items: [
         { icon: "equalizer", title: "Statistics", route: "/" },
@@ -287,16 +289,49 @@ export default {
           iron: "6%",
         },
       ],
+      // Rules
+      numCalsRules: [(value) => (!isNaN(value) && value.length > 0) || "Please enter a numeric value"],
+      descriptionRules: [(desc) => desc.length > 0 || "Description is required"],
+      // Date picker
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      dateFormatted: vm.formatDate(
+        new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+          .toISOString()
+          .substr(0, 10)
+      ),
+      menu1: false,
+      menu2: false,
     };
+  },
+  computed: {
+    computedDateFormatted() {
+      return this.formatDate(this.date);
+    },
+  },
+
+  watch: {
+    date() {
+      this.dateFormatted = this.formatDate(this.date);
+    },
+  },
+
+  methods: {
+    formatDate(date) {
+      if (!date) return null;
+
+      const [year, month, day] = date.split("-");
+      return `${month}/${day}/${year}`;
+    },
+    parseDate(date) {
+      if (!date) return null;
+
+      const [month, day, year] = date.split("/");
+      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.dashboard {
-  // .dashboard__app-name {
-  //     font-size: 25px;
-  //     text-overflow: ellipsis;
-  // }
-}
-</style>
+<style lang="scss" scoped></style>
